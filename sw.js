bdd4dbdb2a273ae60a7e45e75554746e88849a37
Alsalam university <akrammkreef313@gmail.com>
2026-09-18 17:05:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>clients.claim());
+const CACHE='alnajah-v8';
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{
+    if(r.ok){const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c)).catch(()=>undefined);}
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
